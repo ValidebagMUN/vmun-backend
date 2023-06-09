546b82e1e9bed4e8dec3a4f3dae35dbed7d232ae
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-06-09 12:54:14 +0300

    feat: add me action to UserViewSet for frontend utility

--- vmun_backend/views.py
+++ vmun_backend/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .serializers import UserSerializer
 
@@ -9,3 +11,9 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False)
+    def me(self, request):
+        user = request.user
+        serializer = self.get_serializer(user, many=False)
+        return Response(serializer.data)
